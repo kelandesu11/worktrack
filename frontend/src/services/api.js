62354a7306425384a6d28658a4907fe719a86798
Kelan <kelandesu@gmail.com>
2026-04-17 17:28:26 -0400

    Add request interceptor to include authorization token in API requests

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
